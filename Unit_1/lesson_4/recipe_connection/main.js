08d99ef29be55f298a6685175d4977c3d19420ab
sbahlemoyo <157314718+sbahlemoyo@users.noreply.github.com>
2025-07-08 18:56:58 +0200

    updates

--- Unit_1/lesson_4/recipe_connection/main.js
+++
@@ -1,9 +0,0 @@
-
-//Import the 'cities' module which allows lookup of
-const city = require("cities");
-
-//use zip_lookup method to get city information for 
-var myCity = city.zip_lookup("20016");
-
-//output city info to the console
-console.log(myCity);
